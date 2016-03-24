b26da1dc1ad5e35c54ae71ab31ac2f554e1600bb
Kara Yount <kara.m.yount@gmail.com>
2016-03-23 23:01:58 -0700

    server complete

---
+++ server.py
@@ -0,0 +1,43 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+
+messages = []
+
+@app.route('/')
+def show_chat():
+    return render_template("chat-client.html")
+
+
+@app.route('/create_message', methods=['POST'])
+def create_message():
+
+    new_message = request.form.get("message-field")
+    global messages
+    messages.append(new_message)
+
+    return jsonify({})
+
+
+@app.route('/deliver_new_messages.json')
+def deliver_messages():
+
+    global messages
+    return jsonify(messages=messages)
+
+if __name__ == '__main__':
+    app.run()
+
+
+
+curl 127.0.0.1:5000/deliver_new_messages.json
+{
+  "messages": [
+    "I am a string",
+    "'string'",
+    "string"
+  ]
+}
+curl --data "field-name=string" 127.0.0.1:5000/create_message
+{}
